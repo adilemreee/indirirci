f92f2cbe0efbc9fcb5b56e87a2de142ef45b6405
adilemreee <adil.emre@hotmail.com>
2025-02-19 20:24:36 +0300

    yeni

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from urllib.parse import quote  # Doğru modül
 from flask import Flask, render_template, request, Response
 import yt_dlp
 import requests
+import instaloader
 
 app = Flask(__name__)
+INSTAGRAM_COOKIES = "38789470070%3A8Wo5dp7Vta07Rh%3A29%3AAYcBZOzmNNbVrLnf7ciO0GztW9wLXj2mFD0pE_bnjQ"
+
 
 @app.route("/", methods=["GET"])
 def home():
@@ -43,36 +46,35 @@ def youtube():
     return render_template("youtube.html")
 
 
+
 @app.route("/instagram", methods=["GET", "POST"])
 def instagram():
     if request.method == "POST":
         url = request.form.get("url")
+
         try:
             ydl_opts = {
-                'format': 'best[ext=mp4]/best',
-                'noplaylist': True,
-                'nocache' : True,
-                'cachedir': False , # Önbelleği tamamen kapat
-
+                "format": "best[ext=mp4]/best",
+                "noplaylist": True,
+                "nocache": True,
+                "cachedir": False,
+                "quiet": True  # Konsol çıktısını kapat
             }
+
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
-                stream_url = info_dict.get('url', None)
-                title = info_dict.get('title', 'video')
+                stream_url = info_dict.get("url", None)
+                title = info_dict.get("title", "Instagram Video")
 
                 if stream_url:
                     return render_template("stream.html", stream_url=stream_url, platform="Instagram", title=title)
-
                 else:
-                    raise ValueError("Could not retrieve stream URL.")
+                    return render_template("instagram.html", error="Video URL alınamadı.")
 
         except Exception as e:
-            print(f"Instagram Error: {e}")
-            error_message = f"Instagram Error: {e}"
-            return render_template("instagram.html", error=error_message)
+            return render_template("instagram.html", error=f"Hata: {e}")
 
     return render_template("instagram.html")
-
 
 @app.route("/download")
 def download():
